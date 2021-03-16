7cc64338b418c3a3c3e202cee9b86b182ffe85e8
Bruno Cesar <bcesar.bruno@gmail.com>
2021-03-16 14:51:33 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/dev/brunocesar/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/dev/brunocesar/hrworker/resources/WorkerResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ public class WorkerResources {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
 
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
@@ -28,6 +32,12 @@ public class WorkerResources {
 
 	public WorkerResources(WorkerRepository repository) {
 		this.repository = repository;
+	}
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping
@@ -38,7 +48,7 @@ public class WorkerResources {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable("id") Long id) {
-		
+
 //		try {
 //			Thread.sleep(3000);
 //		} catch (InterruptedException e) {
